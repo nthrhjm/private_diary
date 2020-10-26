7a0764dc507280e65f21eb624223cab9fa207281
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-10-26 15:39:05 +0900

    日記一覧ページを作成 ログイン中のナビゲーションバーに日記一覧ページへのリンクを追加 日記モデルのmigrationを実行

--- diary/views.py
+++ diary/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
@@ -23,3 +25,11 @@ class InquiryView(generic.FormView):
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
 
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        return diaries
